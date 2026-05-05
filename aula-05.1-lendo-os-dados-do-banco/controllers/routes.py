6b94a6afc195bb017c876b24e7d6ebab7571243b
NATAN VIEIRA SILVA <natan.silva@e239.local>
2026-05-05 10:39:26 -0300

    lendo-os-dados-no-banco

---
+++ aula-05.1-lendo-os-dados-do-banco/controllers/routes.py
@@ -0,0 +1,75 @@
+# Importando o flask para a aplicação
+from flask import render_template, request, redirect, url_for
+# Criando a função principal para a inicializar as rotas
+#Importando o modo de Games
+from models.database import Game
+
+
+def init_app(app):
+    # Variáveis Globais
+    listaConsoles = ['Playstation 5', 'Xbox One',
+                     'Super Nintendo', 'Atari', '3DS']
+    listaGames = [{'titulo' : 'CS-GO', 'ano' : 2012,
+                   'categoria' : 'FPS Online', 'plataforma' : 'PC(Windows)'}]
+
+    # Criando a rota principal do site
+
+    @app.route('/')
+    # def cria funções no python
+    def home():
+        return render_template('index.html')
+
+    @app.route('/games')
+    def games():
+        # criando variáveis para a rota de games
+        titulo = "Portal 2"
+        ano = 2011
+        categoria = "Puzzle"
+        # Lista de Jogadores(uma lista é um vetor/array)
+        jogadores = ['Marcos', 'Richard', 'Miguel', 'Renato', 'Pedro']
+
+        # Enviando categorias para html
+        return render_template('games.html',
+                               titulo=titulo,
+                               ano=ano,
+                               categoria=categoria,
+                               jogadores=jogadores)
+
+    @app.route('/consoles', methods=['GET', 'POST'])
+    def consoles():
+        # Criando um objeto
+        console = {"Nome": "Playstation 2",
+                   "Fabricante": "Sony",
+                   "Ano": 2000}
+
+        # Recebendo o valor do formulário
+        if request.method == 'POST':
+            if request.form.get('novoConsole'):
+                listaConsoles.append(request.form.get('novoConsole'))
+
+        return render_template('consoles.html',
+                               console=console,
+                               listaConsoles=listaConsoles)
+
+    # Rota para cadastrar o jogo
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+        # Recebendo os dados do formulário e enviando para página
+        # Verificando se a requisição od usuário é do tipo POST
+        if request.method == 'POST':
+            # Aqui ele irá gravar os dados na lista de jogos
+            listaGames.append({'titulo': request.form.get('titulo'), 'ano': request.form.get
+            ('ano'), 'categoria': request.form.get('categoria'), 'plataforma': request.form.get('plataforma')})
+            # .append adiciona um item na lista
+            #Aqui o usuário será redirecionado para a página
+            return redirect(url_for('cadgames'))
+            
+        return render_template('cadgames.html',
+                               listaGames = listaGames)
+        
+    # ROTA PARA O CRUD
+    @app.route('/estoque',methods=['GET','POST'])
+    def estoque():
+        #SECIONANDO TODOS OS JOGOS DA TABELA
+        games = Game.query.all()
+        return render_template('estoque.html', games=games)    
